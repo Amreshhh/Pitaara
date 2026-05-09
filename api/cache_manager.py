0f3b548595587ac7bd3ee17c6d13e89b5afbe44f
Amresh Agarwal <amuamresh12@gmail.com>
2026-05-09 15:40:08 +0530

    changed cron time for testing

--- api/cache_manager.py
+++ api/cache_manager.py
@@ -104,7 +104,7 @@ async def lifespan(app):
     scheduler = AsyncIOScheduler()
     
     # Schedule to run every day at 12:00 PM (Noon)
-    scheduler.add_job(fetch_and_cache_rates, 'cron', hour=12, minute=0)
+    scheduler.add_job(fetch_and_cache_rates, 'cron', hour=3, minute=25)
     scheduler.start()
     print("📅 Scheduler activated - Daily update scheduled at 12:00 PM (Noon)")
     
